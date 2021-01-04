6f3f58d5dc3932d9ed7fd29c69dbf115957f29d3
nhannht <nhanclassroom@gmail.com>
2021-01-04 12:06:17 +0800

    redesign README.oth

--- packages.el
+++ packages.el
@@ -891,4 +891,5 @@
     :config
     (autoload 'poporg-dwim "poporg" nil t)
     (global-set-key (kbd "C-c \"") 'poporg-dwim)))
-
+(defun david/post-init-evil ()
+  (load-file " ~/.emacs.d/private/local/evil-evilified-state/evil-evilified-state.el"))
